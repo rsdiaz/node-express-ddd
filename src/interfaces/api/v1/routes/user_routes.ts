d8f6acae24282948d6a419dc6cc444c83dd1e0b8
rsdiaz <robertoserrano83@gmail.com>
2023-05-07 23:13:42 +0200

    route for get user by id

--- src/interfaces/api/v1/routes/user_routes.ts
+++ src/interfaces/api/v1/routes/user_routes.ts
@@ -4,6 +4,7 @@ import { UserController } from '../controllers/user_controller'
 const userRouter: Router = express.Router()
 const userController = new UserController()
 
+userRouter.get('/:id', userController.findById)
 userRouter.post('/register', userController.register)
 
 export default userRouter
